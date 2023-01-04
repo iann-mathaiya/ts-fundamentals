907cae9d2c558f4ea773685f2d9f25a201fda6f6
Ian Mathaiya <iannmathaiya@gmail.com>
2023-01-04 15:38:46 +0300

    primitive types commit

---
+++ PrimitiveTypes/strings.ts
@@ -0,0 +1,4 @@
+let firstName: string = "Mateo";
+let sentence: string = `My name is ${firstName}.
+    I am new to TypeScript.`;
+console.log(sentence);
---
+++ build/PrimitiveTypes/strings.js
@@ -0,0 +1,5 @@
+"use strict";
+let firstName = "Mateo";
+let sentence = `My name is ${firstName}.
+    I am new to TypeScript.`;
+console.log(sentence);
