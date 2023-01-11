344e0a680e6a3e52ed70b8a498eb495e81fea183
Ian Mathaiya <iannmathaiya@gmail.com>
2023-01-11 08:49:25 +0300

    accessing external type libraries

--- build/Modules/greetings_module.js
+++ build/Modules/greetings_module.js
@@ -18,3 +18,21 @@ exports.returnGreeting = returnGreeting;
 // -> import { <component name> as <new name> } from '<module name>'
 // To import the entire module into a single variable, and use it to access the module exports:
 // -> import * as <variable name> from '<module name>'
+// Compiling Modules
+// Modules import one another using a module loader. At runtime, the module loader 
+// locates and executes all dependencies of a module before executing it. Depending on 
+// the module target that you specify during compilation, the compiler will generate 
+// appropriate code for Node.js (CommonJS), require.js (AMD), UMD, SystemJS, or 
+// ECMAScript 2015 native modules (ES6) module-loading systems. 
+// To compile modules, specify a --module target on the command line or in the 
+// tsconfig.json file for the project.
+// Open the terminal and complie a ts module by typing the following command:
+// -> tsc --module <target> <filepath>
+// For example:
+// -> tsc --module commonjs main.ts (targets nodejs)
+// Running modules from a web page
+// If you want to instead compile the TypeScript file for 
+// ES6 for use in a web browser, type the following command:
+// -> tsc --module es6 main.ts
+// To run a module from a web page, remember to set the type option to "module":
+// -> <script type="module" src=".\main.js"></script>
